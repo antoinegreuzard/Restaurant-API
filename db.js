c9fd2c98c96b70bf0e7142a2d4d2a8c113ef39f9
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-02-24 08:15:49 +0100

    feat: use mysql2 instead of mysql

--- db.js
+++ db.js
@@ -1,20 +1,21 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  port: '3306',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 db.connect(function (err) {
-    if (err) {
-        console.error('Error connecting: ' + err.stack);
-        return;
-    }
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
 
-    console.log('Connected as id ' + db.threadId);
+  console.log('Connected as id ' + db.threadId);
 });
 
 module.exports = db;
