393ed27488530d4a5bf07ae9625ea18fca55cb0c
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-02-24 09:45:43 +0100

    fix: utf8mb4_general_ci

--- restapi.sql
+++ restapi.sql
@@ -33,7 +33,7 @@ CREATE TABLE `categories`
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 3
   DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+  COLLATE = utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -54,7 +54,7 @@ CREATE TABLE `formulas`
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 5
   DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+  COLLATE = utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
