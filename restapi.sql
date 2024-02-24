19cc0eabc024a433f0ba09f8d7e1a37fc30c46b3
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-02-24 09:42:32 +0100

    fix: use utf8mb4_general_ci collation

--- restapi.sql
+++ restapi.sql
@@ -78,7 +78,7 @@ CREATE TABLE `items`
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 11
   DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+  COLLATE = utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
 
